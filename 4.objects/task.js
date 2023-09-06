653f8ceaea6aa3ac6cce88f7404ef182f0b8d2c8
Nataliia Bulatovskaia <dnatali2211@gmail.com>
2023-09-06 11:42:11 +0400

    Realised fourth task

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+}
+let student1 = new Student("Александр", "мужской", 31);
+let student2 = new Student("Мария", "женский", 27);
+let student3 = new Student("Анатолий", "мужской", 33);
+
+Student.prototype.setSubject = function(subjectName) {
+    this.subject = subjectName;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.addMarks = function(...marksToAdd) {
+    if (!this.marks) {
+        console.log(`${this.name} отчислен.`);
+    } else {
+        this.marks.push(...marksToAdd);
+    }
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.getAverage = function() {
+    if (!this.marks || this.marks.length === 0) {
+        return 0;
+    } else {
+        return this.marks.reduce((acc, mark) => acc + mark, 0) / this.marks.length;
+    }
 }
 
-Student.prototype.getAverage = function () {
-  
-}
-
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }

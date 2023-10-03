f093ed65c3c517d8dcad2b00637972a7b5e9be90
Nataliia Bulatovskaia <dnatali2211@gmail.com>
2023-10-03 09:38:41 +0400

    Realised eighth task

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,52 @@
+'use strict';
 //Задача № 1
+const md5 = require('js-md5');
+
 function cachingDecoratorNew(func) {
-  
+    let cache = [];
+
+    function wrapper(...args) {
+        const hash = md5(JSON.stringify(args));
+        let objectInCache = cache.find(item => item.hash === hash);
+        if (objectInCache) {
+            console.log("Из кеша: " + objectInCache.value, cache);
+            return "Из кеша: " + objectInCache.value;
+        }
+        let result = func(...args);
+        cache.push({
+            hash: hash,
+            value: result
+        });
+        if (cache.length > 5) {
+            cache.shift();
+        }
+        console.log("Вычисляем " + result, cache);
+        return "Вычисляем " + result;
+    }
+    return wrapper;
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+    let timeoutId;
+    let count = 0;
+    let allCount = 0;
+
+    function wrapper(...args) {
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+            allCount++;
+        }
+        if (!timeoutId) {
+            func.apply(this, args);
+            count++;
+        }
+        timeoutId = setTimeout(() => {
+            count++;
+            func.apply(this, args);
+            wrapper.count = count;
+        }, delay);
+        wrapper.allCount = allCount;
+    }
+    return wrapper;
 }
